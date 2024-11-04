788889646dfb054845f45fb75d12417380203bf3
NicoVenturin <145157147+NicoVenturin@users.noreply.github.com>
2024-11-04 19:52:54 -0300

    Update obteenerProductos.js

--- public/js/obteenerProductos.js
+++ public/js/obteenerProductos.js
@@ -1,4 +1,4 @@
-const respuesta = fetch('http://localhost:3000/productos')
+const respuesta = fetch('https://hardtienda.onrender.com/productos')
 .then(respuesta => respuesta.json())
 //respuesta.then(datos=>console.log(datos));
 .then(datos => mostrarProductos(datos));
